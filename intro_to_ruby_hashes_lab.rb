111efb79403e2e7271b9cf2aab4369af0d99898d
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-14 00:24:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  hash = {:railroads => {pieces: 4, :rent_in_dollars => {}}}
+  hash = {:railroads => {pieces: 4, :rent_in_dollars => {}, :names => {}}}
   
 end
 
