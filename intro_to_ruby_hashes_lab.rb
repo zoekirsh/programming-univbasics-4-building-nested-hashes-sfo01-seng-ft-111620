c782c3c1ac6df0c9926fadb621124435b268e955
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-14 00:31:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,10 +15,15 @@ def monopoly_with_third_tier
       }
     }
   }
-
 end
 
 def monopoly_with_fourth_tier
-  
-
+  hash = {:railroads => {
+    pieces: 4, 
+    :rent_in_dollars => {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned: 200}, 
+    :names => {
+      :reading_railroad => {mortgage_value: 100}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline_railroad => {}
+      }
+    }
+  }
 end
