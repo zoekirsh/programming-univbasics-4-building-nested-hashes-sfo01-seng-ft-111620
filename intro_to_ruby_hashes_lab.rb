ff1c0370a7407753823c651805b44914a4eb7ca9
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-14 00:26:20 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -9,7 +9,7 @@ end
 def monopoly_with_third_tier
   hash = {:railroads => {
     pieces: 4, 
-    :rent_in_dollars => {}, 
+    :rent_in_dollars => {one_piece_owned: 25}, 
     :names => {}
     }
   }
